aaa67320e7b906293fd94eb262cd2b71ab8eb920
Maksim Kostromin <maksim@beachfront.com>
2023-05-19 17:51:53 -0400

    Initial commit

---
+++ config-props/src/test/java/io/github/daggerok/springprops/SpringPropsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.daggerok.springprops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPropsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
